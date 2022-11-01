25e3790802e987356450bae1845e17b6f7f05ad0
Moridan <daan_severijns@hotmail.com>
2022-11-01 11:25:15 +0100

    Test

--- fabrikanten/models.py
+++ fabrikanten/models.py
@@ -5,3 +5,8 @@ from django.db import models
 
 class Fabrikanten(models.Model):
     name = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = "fabrikanten"
+    def __str__(self):
+        return self.name
